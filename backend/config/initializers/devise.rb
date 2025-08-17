9680d129b0ec558fc99212fef7dcff5a290dabc3
AzizOmri2 <aziz.omri@esprit.tn>
2025-08-17 14:57:58 +0100

    V69 - Update devise.rb

--- backend/config/initializers/devise.rb
+++ backend/config/initializers/devise.rb
@@ -21,7 +21,7 @@ Devise.setup do |config|
   config.jwt do |jwt|    
     jwt.secret = Rails.application.credentials.secret_key_base
     jwt.dispatch_requests = [
-      ['POST', %r{^/users$}]
+      ['POST', %r{^/users/sign_in$}]
     ]
     jwt.revocation_requests = [
       ['DELETE', %r{^/users$}]
